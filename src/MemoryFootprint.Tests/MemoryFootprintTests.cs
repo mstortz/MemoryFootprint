50684154f5e0e39f373c4a381efdf12467059a92
Mike Stortz <mstortz@urvin.finance>
2024-05-26 18:47:42 -0400

    0.01 - MHS - Initial Commit

---
+++ src/MemoryFootprint.Tests/MemoryFootprintTests.cs
@@ -0,0 +1,226 @@
+namespace MemoryFootprint.Tests;
+
+using Extensions.Object;
+using FluentAssertions;
+
+public class MemoryFootprintTests
+{
+    [Fact]
+    public void SizeOfSbyte_EqualsOne()
+    {
+        const sbyte mySbyte = 2;
+        long size = mySbyte.MemoryFootprint();
+        Assert.Equal(1, size);
+    }
+
+    [Fact]
+    public void SizeOfChar_EqualsTwo()
+    {
+        const char myChar = ' ';
+        long size = myChar.MemoryFootprint();
+        Assert.Equal(1, size);
+    }
+
+    [Fact]
+    public void SizeOfDecimal_EqualsFour()
+    {
+        const decimal myDecimal = 2;
+        long size = myDecimal.MemoryFootprint();
+        Assert.Equal(16, size);
+    }
+
+    [Fact]
+    public void SizeOfDouble_EqualsEight()
+    {
+        const double myDouble = 2;
+        long size = myDouble.MemoryFootprint();
+        Assert.Equal(8, size);
+    }
+
+    [Fact]
+    public void SizeOfFloat_EqualsFour()
+    {
+        const float myFloat = 2;
+        long size = myFloat.MemoryFootprint();
+        Assert.Equal(4, size);
+    }
+
+    [Fact]
+    public void SizeOfInt_EqualsFour()
+    {
+        const int myInt = 2;
+        long size = myInt.MemoryFootprint();
+        Assert.Equal(4, size);
+    }
+
+    [Fact]
+    public void SizeOfUint_EqualsFour()
+    {
+        const uint myUint = 2;
+        long size = myUint.MemoryFootprint();
+        Assert.Equal(4, size);
+    }
+
+    [Fact]
+    public void SizeOfNint_EqualsEight()
+    {
+        nint myNint = 2;
+        long size = myNint.MemoryFootprint();
+        Assert.Equal(8, size);
+    }
+
+    [Fact]
+    public void SizeOfNuint_EqualsEight()
+    {
+        nuint myNuint = 2;
+        long size = myNuint.MemoryFootprint();
+        Assert.Equal(8, size);
+    }
+
+    [Fact]
+    public void SizeOfLong_EqualsEight()
+    {
+        const long myLong = 2;
+        long size = myLong.MemoryFootprint();
+        Assert.Equal(8, size);
+    }
+
+    [Fact]
+    public void SizeOfUlong_EqualsEight()
+    {
+        const ulong myUlong = 2;
+        long size = myUlong.MemoryFootprint();
+        Assert.Equal(8, size);
+    }
+
+    [Fact]
+    public void SizeOfShort_EqualsTwo()
+    {
+        const short myShort = 2;
+        long size = myShort.MemoryFootprint();
+        Assert.Equal(2, size);
+    }
+
+    [Fact]
+    public void SizeOfUshort_EqualsTwo()
+    {
+        const ushort myUshort = 2;
+        long size = myUshort.MemoryFootprint();
+        Assert.Equal(2, size);
+    }
+
+    struct MyStruct
+    {
+        public int MyInt;
+        public char MyChar;
+        public string MyString;
+    }
+
+    [Fact]
+    public void SizeOfMyStruct_EqualsTwentyThree()
+    {
+        MyStruct myStruct = new MyStruct {  MyChar = ' ', MyInt = 2, MyString = "Hello" };
+        long size = myStruct.MemoryFootprint();
+        Assert.Equal(23, size);
+    }
+
+    class MyClass
+    {
+        public int MyInt;
+        public char MyChar;
+        public string MyString = default!;
+    }
+
+    [Fact]
+    public void SizeOfMyClass_EqualsTwentyThree()
+    {
+        MyClass myClass = new MyClass { MyChar = ' ', MyInt = 2, MyString = "Hello" };
+        long size = myClass.MemoryFootprint();
+        Assert.Equal(23, size);
+    }
+
+    class MyReferenceClass
+    {
+        public string String1 = default!;
+        public string String2 = default!;
+    }
+
+    [Fact]
+    public void SizeOfMyReferenceClass_ShouldNotEqual()
+    {
+        const string s1 = "This is a test string";
+        const string s2 = "This is a cool string";
+        MyReferenceClass myRefClass1 = new MyReferenceClass { String1 = s1, String2 = s1 };
+        MyReferenceClass myRefClass2 = new MyReferenceClass { String1 = s1, String2 = s2 };
+        long size1 = myRefClass1.MemoryFootprint();
+        long size2 = myRefClass2.MemoryFootprint();
+        size1.Should().Be(50);
+        size2.Should().Be(100);
+        size1.Should().NotBe(size2);
+    }
+
+    public class MyRecursiveClass
+    {
+        public MyRecursiveClass Recurse = default!;
+    }
+
+    [Fact]
+    public void RecursiveClass_ShouldNotThrow()
+    {
+        var c1 = new MyRecursiveClass();
+        var c2 = new MyRecursiveClass();
+        c1.Recurse = c2;
+        c2.Recurse = c1;
+        long size = 0;
+        c1.Invoking(x => size = x.MemoryFootprint()).Should().Throw<ArgumentException>();
+    }
+}
+
+public class ObjectExtensionsTests
+{
+    [Fact]
+    public void MemoryFootprint_WithNullObject_ShouldReturnZero()
+    {
+        object? obj = null;
+        long size = obj.MemoryFootprint();
+        size.Should().Be(0);
+    }
+
+    [Fact]
+    public void MemoryFootprint_WithBuiltInType_ShouldReturnSizeOfBuiltInType()
+    {
+        const int obj = 123;
+        long size = obj.MemoryFootprint();
+        size.Should().Be(sizeof(int));
+    }
+
+    [Fact]
+    public void MemoryFootprint_WithArray_ShouldReturnSizeOfArray()
+    {
+        int[] obj = [1, 2, 3];
+        long size = obj.MemoryFootprint();
+        size.Should().BeGreaterOrEqualTo(obj.Length * sizeof(int));
+    }
+
+    [Fact]
+    public void MemoryFootprint_WithCollection_ShouldReturnSizeOfCollection()
+    {
+        var obj = new List<int> { 1, 2, 3 };
+        long size = obj.MemoryFootprint();
+        size.Should().BeGreaterOrEqualTo(obj.Count * sizeof(int));
+    }
+
+    [Fact]
+    public void MemoryFootprint_WithCustomObject_ShouldReturnSizeOfObject()
+    {
+        var obj = new CustomObject { Property1 = "Test", Property2 = 123 };
+        long size = obj.MemoryFootprint();
+        size.Should().BeGreaterOrEqualTo(sizeof(int) + (sizeof(char) * obj.Property1.Length));
+    }
+
+    private class CustomObject
+    {
+        public string Property1 { get; set; } = "Hello";
+        public int Property2 { get; set; }
+    }
+}
---
+++ src/MemoryFootprint/MemoryFootprint.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace Extensions.Object;
+
+public static class ObjectExtensions
+{
+    private static readonly HashSet<Type> _builtInTypes =
+    [
+        typeof(bool), typeof(byte), typeof(sbyte), typeof(char), typeof(decimal), typeof(double), typeof(float),
+        typeof(int), typeof(uint), typeof(long), typeof(ulong), typeof(short), typeof(ushort), typeof(IntPtr),
+        typeof(UIntPtr), typeof(string)
+    ];
+
+    public static long MemoryFootprint(this object? obj)
+    {
+        // Visited objects are stored in a HashSet to avoid double counting
+        var visited = new HashSet<object>(new ReferenceEqualityComparer());
+
+        // Stack is used to detect cyclic references
+        var stack = new HashSet<object>(new ReferenceEqualityComparer());
+
+        return MemoryFootprint(obj, visited, stack);
+    }
+
+    private static long MemoryFootprint(object? obj, HashSet<object> visited, HashSet<object> stack)
+    {
+        if (obj == null)
+            return 0;
+
+        Type type = obj.GetType();
+
+        if (type == typeof(string))
+        {
+            if (visited.Contains(obj))
+            {
+                return 0;
+            }
+            visited.Add(obj);
+            return IntPtr.Size + (((string)obj).Length * sizeof(char));
+        }
+
+        if (_builtInTypes.Contains(type))
+        {
+            return Marshal.SizeOf(type);
+        }
+
+        var isVisited = visited.Contains(obj);
+        if (isVisited)
+        {
+            if (stack.Contains(obj))
+            {
+                throw new ArgumentException("Cyclic reference detected.");
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        visited.Add(obj);
+        stack.Add(obj);
+
+        long size = 0;
+
+        if (typeof(IList).IsAssignableFrom(type))
+        {
+            size = IntPtr.Size; // Pointer to the list
+
+            foreach (var item in (IList)obj)
+            {
+                size += MemoryFootprint(item, visited, stack);
+            }
+
+            stack.Remove(obj);
+            return size;
+        }
+
+        // Calculate size of all fields
+        foreach (FieldInfo field in type.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public))
+        {
+            object? fieldValue = field.GetValue(obj);
+            size += MemoryFootprint(fieldValue, visited, stack);
+        }
+
+        // Calculate size of all properties
+        foreach (PropertyInfo property in type.GetProperties(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public))
+        {
+            if (property.GetIndexParameters().Length == 0) // Skip indexers
+            {
+                object? propertyValue = property.GetValue(obj);
+                size += MemoryFootprint(propertyValue, visited, stack);
+            }
+        }
+
+        stack.Remove(obj);
+        return size;
+    }
+
+    private sealed class ReferenceEqualityComparer : IEqualityComparer<object>
+    {
+        // Static so that the two comparers share the same hash codes
+        private static readonly Dictionary<object, int> _hashCodes = [];
+
+        public new bool Equals(object? x, object? y)
+        {
+            return ReferenceEquals(x, y);
+        }
+
+        public int GetHashCode(object obj)
+        {
+            if (!_hashCodes.TryGetValue(obj, out var value))
+            {
+                value = System.Runtime.CompilerServices.RuntimeHelpers.GetHashCode(obj);
+                _hashCodes[obj] = value;
+            }
+            return value;
+        }
+    }
+}
